972e8978c22b55e472c373e4f9a4c7c4b490f9fb
Francisco <panchi_nrc@hotmail.com>
2021-11-26 16:06:04 -0300

    cambios generales

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,70 +1,72 @@
-import React from 'react'
-import Twitter from '../../assets/img/gorjeo.svg'
-import Github from '../../assets/img/github.svg'
-import Email from '../../assets/img/email.svg'
-import './Footer.css'
-import Typical from 'react-typical'
+import React from "react";
+import Twitter from "../../assets/img/gorjeo.svg";
+import Github from "../../assets/img/github.svg";
+import Email from "../../assets/img/email.svg";
+import "./Footer.css";
+import Typical from "react-typical";
+import Linkedin from "../../assets/img/linkedin-icon-2.svg";
 
-
-
-
-
-
-function Footer () {
-
-
-    return (
-        <div className="d-flex flex-column align-items-center justify-content-center">
-        <div className="container ">
-
-          <div className="footer ">
-              <div className="row">
-                  <div className="col-12 col-md-6 offset-md-3 text-left">
-
-        <h1 className="text-monospace text-break text-center ">
-          {' '}
-          <Typical
-          
-          wrapper='p'
-          steps={[
-            ' Contacto',
-            3000
-           
-          ]}
-          />
-        </h1>
-                  </div>
-              </div>
-            <div className="row w-100">
-                <div className="col-12 col-md-6 offset-md-3 ">
-
-                <ul className="d-flex flex-row justify-content-between lista-footer ">
-                    <li>
-                       <a target="_blank" rel="noopener noreferrer"  href="//twitter.com/@panchisepulveda"> <img src={Twitter} alt="Twitter logo"/></a>
-                    </li>
-                    <li>
-                       <a target="_blank" rel="noopener noreferrer" href="https://github.com/panchixnrc"><img  src={Github} alt="Github logo"/></a> 
-
-                    </li>
-                        <li>
-                            <a href="mailto:panchinrc1993@gmail.com"><img src={Email} alt="Email logo"/></a>
-                            
-                            </li>
-                </ul>
-                </div>
-
+function Footer() {
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center">
+      <div className="container ">
+        <div className="footer ">
+          <div className="row">
+            <div className="col-12 col-md-6 offset-md-3 text-left">
+              <h1 className="text-monospace text-break text-center ">
+                {" "}
+                <Typical wrapper="p" steps={[" Contacto", 3000]} />
+              </h1>
             </div>
-            <div className="row">
-                <div className="col-12 text-center mt-4">
-                <h5>Made with <span role="img">💚</span> for <a target="_blank" rel="noopener noreferrer" href="https://github.com/panchixnrc">Francisco Sepulveda</a></h5>
-
-                </div>
+          </div>
+          <div className="row w-100">
+            <div className="col-12 col-md-6 offset-md-3 ">
+              <ul className="d-flex flex-row justify-content-between lista-footer ">
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.linkedin.com/in/fsepulvedadev"
+                  >
+                    {" "}
+                    <img src={Linkedin} alt="Linkedin logo" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/panchixnrc"
+                  >
+                    <img src={Github} alt="Github logo" />
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto:fsepulvedadev@gmail.com">
+                    <img src={Email} alt="Email logo" />
+                  </a>
+                </li>
+              </ul>
             </div>
+          </div>
+          <div className="row">
+            <div className="col-12 text-center mt-4">
+              <h5>
+                Made with <span role="img">💚</span> for{" "}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/panchixnrc"
+                >
+                  Francisco Sepulveda
+                </a>
+              </h5>
+            </div>
+          </div>
         </div>
-        </div>
-
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Proyectos/Proyectos.js
+++ src/pages/Proyectos/Proyectos.js
@@ -1,212 +1,248 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Typical from 'react-typical'
-import './Proyectos.css'
-import { Card, CardGroup } from 'react-bootstrap'
-import sya from '../../assets/img/syaweb.png'
-import modular from '../../assets/img/modularweb.png'
-import Github from '../../assets/img/github.svg'
-import website from '../../assets/img/web.svg'
-import html5  from '../../assets/img/html5.svg'
-import css  from '../../assets/img/css3.svg'
-import node from '../../assets/img/node.svg'
-import bootstrap from '../../assets/img/bootstrap.svg'
-import js from '../../assets/img/javascript.svg'
-import portfolio from '../../assets/img/screen-portfolio.png'
-import ReactIcon from '../../assets/img/icon-react.svg'
-
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Typical from "react-typical";
+import "./Proyectos.css";
+import { Card, CardGroup } from "react-bootstrap";
+import sya from "../../assets/img/syaweb.png";
+import modular from "../../assets/img/modularweb.png";
+import Github from "../../assets/img/github.svg";
+import website from "../../assets/img/web.svg";
+import html5 from "../../assets/img/html5.svg";
+import css from "../../assets/img/css3.svg";
+import node from "../../assets/img/node.svg";
+import bootstrap from "../../assets/img/bootstrap.svg";
+import js from "../../assets/img/javascript.svg";
+import tareas from "../../assets/img/Tareas app.png";
+import ReactIcon from "../../assets/img/icon-react.svg";
+import TailwindIcon from "../../assets/img/tailwindcss.svg";
 
 function App() {
   return (
-    <div className=" d-flex justify-content-center align-items-center flex-column" >
-
-    
-    <div className="container">
-    <div className="row main-proyect d-none d-md-flex justify-content-center align-items-end">
-        <div className="col">
-
-        
-        <h1 className="text-monospace text-break text-center">
-          {' '}
-          <Typical
-          
-          wrapper='p'
-          steps={[
-            ' Portfolio',
-            3000
-           
-          ]}
-          />
-        </h1>
-        </div>
-    </div>
-    <div className="row main-proyect submain-proyect">
-
-        <div className="col">
-        <h1 className="text-monospace text-break text-center align-self-start">
-          {' '}
-          <Typical
-          loop={Infinity}
-          wrapper='p'
-          steps={[
-            ' Estos son algunos de mis proyectos',
-            3000,
-            'Estoy trabajando en varios más 🚀',
-            3000
-           
-          ]}
-          />
-        </h1>
-        </div>
-    </div>
-          <div className="row cards align-items-start justify-content-around text-monospace">
-            <CardGroup>
-            <div className=" col-12 col-md-4 ">
-                      <Card bg="dark" border="success" style={{ width: '20rem' }}>
-              <Card.Img className="green-filter" variant="top" src={sya} />
-              <Card.Body>
-                <Card.Title>Website S & A</Card.Title>
-                <Card.Text>
-                 Landing page para empresa de transporte, desarrollado con HTLM, CSS, BOOTSTRAP en el backend Node.js con NodeMailer.
-
-                </Card.Text>
-                <Card.Text>
-
-                <hr/>
-                <ul className="list-inline lista-tec d-flex justify-content-around align-items-center">
-                  <li className="list-inline-item">
-                      <img src={html5} alt="html"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={css} alt="css"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={js} alt="javascript"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={node} alt="node"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={bootstrap} alt="bootstrap"/>
-                  </li>
-                  
-                </ul>
-               
-                <hr/>
-                </Card.Text>
-               
-               <ul className="list-inline d-flex justify-content-around lista-proyect">
-                 
-                 <li className="list-inline-item">
-                       <a target="_blank" rel="noopener noreferrer" href="https://github.com/panchixnrc/web-sya"><img style={{ width: "25px"}} src={Github} alt="Github logo"/>{' '}<span>Repo</span></a> 
-
-                    </li>
-                 <li className="list-inline-item">
-                       <a target="_blank" rel="noopener noreferrer" href="https://web-sya-mailer.herokuapp.com/"><img style={{ width: "25px"}} src={website} alt="Live demo" />{' '}<span>Live demo</span></a> 
-
-                    </li>
-                
-               </ul>
-              </Card.Body>
-            </Card>
-            </div>
-            <div className="col-12 col-md-4 ">
-                      <Card bg="dark" border="success" style={{ width: '20rem' }}>
-              <Card.Img className="green-filter" variant="top" src={modular} />
-              <Card.Body>
-                <Card.Title>Website Modular Espacios</Card.Title>
-                <Card.Text>
-                 Web para emprendimiento de construcción con contenedores. Cuenta con un formulario de contacto con NodeMailer y una galería para mostrar productos.
-                </Card.Text>
-                <hr/>
-                <ul className="list-inline lista-tec d-flex justify-content-around align-items-center">
-                  <li className="list-inline-item">
-                      <img src={html5} alt="html"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={css} alt="css"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={js} alt="javascript"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={node} alt="node"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={bootstrap} alt="bootstrap"/>
-                  </li>
-                  
-                </ul>
-                <hr/>
-               <ul className="list-inline d-flex justify-content-around lista-proyect">
-                 
-                 <li className="list-inline-item">
-                       <a target="_blank" rel="noopener noreferrer" href="https://github.com/panchixnrc/modularweb"><img style={{ width: "25px"}} src={Github} alt="Github logo"/>{' '}<span>Repo</span></a> 
-
-                    </li>
-                 <li className="list-inline-item">
-                       <a target="_blank" rel="noopener noreferrer" href="https://www.modularneuquen.com"><img style={{ width: "25px"}} src={website} alt="Live demo" />{' '}<span>Live demo</span></a> 
-
-                    </li>
-                
-               </ul>
-              </Card.Body>
-            </Card>
-            </div>
-            <div className="col-12 col-md-4 ">
-                      <Card bg="dark" border="success" style={{ width: '20rem' }}>
-              <Card.Img className="green-filter" variant="top" src={portfolio} />
-              <Card.Body>
-                <Card.Title>Este Portfolio</Card.Title>
-                <Card.Text>
-                 Este portfolio está desarrollado en React, usando varias de sus herramientas como React Router, para los estilos utiliza Bootstrap y para el contacto Netlify Forms.
-                </Card.Text>
-                <hr/>
-                <ul className="list-inline lista-tec d-flex justify-content-around align-items-center">
-                  <li className="list-inline-item">
-                      <img src={ReactIcon} alt="React"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={css} alt="css"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={js} alt="javascript"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={node} alt="node"/>
-                  </li>
-                  <li className="list-inline-item">
-                      <img src={bootstrap} alt="bootstrap"/>
-                  </li>
-                  
-                </ul>
-                <hr/>
-               <ul className="list-inline d-flex justify-content-around lista-proyect">
-                 
-                 <li className="list-inline-item">
-                       <a target="_blank" rel="noopener noreferrer" href="https://github.com/panchixnrc/portfolio-react"><img style={{ width: "25px"}} src={Github} alt="Github logo"/>{' '}<span>Repo</span></a> 
-
-                    </li>
-                 <li className="list-inline-item">
-                       <a target="_blank" rel="noopener noreferrer" href="https://web-sya-mailer.herokuapp.com/"><img style={{ width: "25px"}} src={website} alt="Live demo" />{' '}<span>Live demo</span></a> 
-
-                    </li>
-                
-               </ul>
-              </Card.Body>
-            </Card>
-            </div>
-         
-
-        </CardGroup>
-
-
+    <div className=" d-flex justify-content-center align-items-center flex-column">
+      <div className="container">
+        <div className="row main-proyect d-none d-md-flex justify-content-center align-items-end">
+          <div className="col">
+            <h1 className="text-monospace text-break text-center">
+              {" "}
+              <Typical wrapper="p" steps={[" Portfolio", 3000]} />
+            </h1>
           </div>
-         
+        </div>
+        <div className="row main-proyect submain-proyect">
+          <div className="col">
+            <h1 className="text-monospace text-break text-center align-self-start">
+              {" "}
+              <Typical
+                loop={Infinity}
+                wrapper="p"
+                steps={[
+                  " Estos son algunos de mis proyectos",
+                  3000,
+                  "Estoy trabajando en varios más 🚀",
+                  3000,
+                ]}
+              />
+            </h1>
+          </div>
+        </div>
+        <div className="row cards align-items-start justify-content-around text-monospace">
+          <CardGroup>
+            <div className=" col-12 col-md-4 ">
+              <Card bg="dark" border="success" style={{ width: "20rem" }}>
+                <Card.Img className="green-filter" variant="top" src={sya} />
+                <Card.Body>
+                  <Card.Title>Website S & A</Card.Title>
+                  <Card.Text>
+                    Landing page para empresa de transporte, desarrollado con
+                    HTLM, CSS, BOOTSTRAP en el backend Node.js con NodeMailer.
+                  </Card.Text>
+                  <Card.Text>
+                    <hr />
+                    <ul className="list-inline lista-tec d-flex justify-content-around align-items-center">
+                      <li className="list-inline-item">
+                        <img src={html5} alt="html" />
+                      </li>
+                      <li className="list-inline-item">
+                        <img src={css} alt="css" />
+                      </li>
+                      <li className="list-inline-item">
+                        <img src={js} alt="javascript" />
+                      </li>
+                      <li className="list-inline-item">
+                        <img src={node} alt="node" />
+                      </li>
+                      <li className="list-inline-item">
+                        <img src={bootstrap} alt="bootstrap" />
+                      </li>
+                    </ul>
+
+                    <hr />
+                  </Card.Text>
+
+                  <ul className="list-inline d-flex justify-content-around lista-proyect">
+                    <li className="list-inline-item">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/panchixnrc/web-sya"
+                      >
+                        <img
+                          style={{ width: "25px" }}
+                          src={Github}
+                          alt="Github logo"
+                        />{" "}
+                        <span>Repo</span>
+                      </a>
+                    </li>
+                    <li className="list-inline-item">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://web-sya-mailer.herokuapp.com/"
+                      >
+                        <img
+                          style={{ width: "25px" }}
+                          src={website}
+                          alt="Live demo"
+                        />{" "}
+                        <span>Live demo</span>
+                      </a>
+                    </li>
+                  </ul>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-12 col-md-4 ">
+              <Card bg="dark" border="success" style={{ width: "20rem" }}>
+                <Card.Img
+                  className="green-filter"
+                  variant="top"
+                  src={modular}
+                />
+                <Card.Body>
+                  <Card.Title>Website Modular Espacios</Card.Title>
+                  <Card.Text>
+                    Web para emprendimiento de construcción con contenedores.
+                    Cuenta con un formulario de contacto con NodeMailer y una
+                    galería para mostrar productos.
+                  </Card.Text>
+                  <hr />
+                  <ul className="list-inline lista-tec d-flex justify-content-around align-items-center">
+                    <li className="list-inline-item">
+                      <img src={html5} alt="html" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={css} alt="css" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={js} alt="javascript" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={node} alt="node" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={bootstrap} alt="bootstrap" />
+                    </li>
+                  </ul>
+                  <hr />
+                  <ul className="list-inline d-flex justify-content-around lista-proyect">
+                    <li className="list-inline-item">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/panchixnrc/modularweb"
+                      >
+                        <img
+                          style={{ width: "25px" }}
+                          src={Github}
+                          alt="Github logo"
+                        />{" "}
+                        <span>Repo</span>
+                      </a>
+                    </li>
+                    <li className="list-inline-item">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.modularneuquen.com"
+                      >
+                        <img
+                          style={{ width: "25px" }}
+                          src={website}
+                          alt="Live demo"
+                        />{" "}
+                        <span>Live demo</span>
+                      </a>
+                    </li>
+                  </ul>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-12 col-md-4 ">
+              <Card bg="dark" border="success" style={{ width: "20rem" }}>
+                <Card.Img className="green-filter" variant="top" src={tareas} />
+                <Card.Body>
+                  <Card.Title>Tareas App</Card.Title>
+                  <Card.Text>
+                    Web app para gestion de tareas realizada con React.js y
+                    Tailwind.css
+                  </Card.Text>
+                  <hr />
+                  <ul className="list-inline lista-tec d-flex justify-content-around align-items-center">
+                    <li className="list-inline-item">
+                      <img src={ReactIcon} alt="React" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={css} alt="css" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={js} alt="javascript" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={node} alt="node" />
+                    </li>
+                    <li className="list-inline-item">
+                      <img src={TailwindIcon} alt="tailwind" />
+                    </li>
+                  </ul>
+                  <hr />
+                  <ul className="list-inline d-flex justify-content-around lista-proyect">
+                    <li className="list-inline-item">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/panchixnrc/tareas-app"
+                      >
+                        <img
+                          style={{ width: "25px" }}
+                          src={Github}
+                          alt="Github logo"
+                        />{" "}
+                        <span>Repo</span>
+                      </a>
+                    </li>
+                    <li className="list-inline-item">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://tareasappfs.netlify.app/"
+                      >
+                        <img
+                          style={{ width: "25px" }}
+                          src={website}
+                          alt="Live demo"
+                        />{" "}
+                        <span>Live demo</span>
+                      </a>
+                    </li>
+                  </ul>
+                </Card.Body>
+              </Card>
+            </div>
+          </CardGroup>
+        </div>
       </div>
-      </div>
-    
+    </div>
   );
 }
 

6542de8995625a879362154946c2f55fb7fb2507
Francisco Sepulveda <fsepulveda@copade.neuquen.gov.ar>
2022-09-19 13:46:01 -0300

    shuting down2

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
-import React from 'react';
-import './App.css';
-import Typical from 'react-typical'
-import {Col, Row, } from 'react-bootstrap'
-
+import React from "react";
+import "./App.css";
+import Typical from "react-typical";
+import { Col, Row } from "react-bootstrap";
 
 function App() {
-
-
-  return (
-  
-
-      <Row className="px-0 m-0 w-100">
-          <Col className="mt-4 p-0 mx-2">
-
-      <div className="App text-left pl-4">
-     
-        <div className="d-flex justify-content-center align-items-center main">
-
-          <h1 className="d-none d-xl-block">
-            <span className="blueConsole">
-
-              fsepulveda@FS~${' '} 
-            </span>
-        </h1> 
-        
-
-        <h1 className="text-monospace text-break">
-          {' '}
-          <Typical
-          loop={Infinity}
-          wrapper='b'
-          steps={[
-            'Hola! Soy Francisco;',
-            1000,
-            'Estoy aprendiendo programación;',
-            1000,
-            'De forma autodidacta hace 1 año; ',
-            1000,
-            'Estoy en búsqueda laboral activa! 🤞',
-            2000
-          ]}
-          />
-        </h1>
-        </div>
-  
-      </div>
-          </Col>
-      </Row>
-
-    );
+  return <div></div>;
 }
 
 export default App;
